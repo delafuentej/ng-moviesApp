cddd2d6c320b8b4b159144d3d09e28e13c8ffe53
Jesus de la Fuente <jfuentelucas@gmail.com>
2022-07-02 17:17:29 +0200

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { SearchComponent } from './pages/search/search.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: HomeComponent},
+  { path:'search/:searchTerm', component:SearchComponent},
+  { path:'movie/:id', component: MovieDetailsComponent},
+  { path:'**', pathMatch:'full', redirectTo:'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
