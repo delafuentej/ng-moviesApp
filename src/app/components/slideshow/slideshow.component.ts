e0fece207d2cfd66973b727bb3d9adc1e7fdf1c9
Jesus de la Fuente <jfuentelucas@gmail.com>
2022-07-02 21:39:37 +0200

    slideshow with swiper

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 import { Movie } from '../../interfaces/film-showing-response';
 
 @Component({
@@ -6,10 +7,20 @@ import { Movie } from '../../interfaces/film-showing-response';
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies:Movie[]=[];
   constructor() { }
+
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      
+      loop: true,
+    
+      
+    });
+  }
 
   ngOnInit(): void {
     console.log(this.movies)
